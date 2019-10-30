6a44432d2f913310868f5c0a41f3fe8b5a565697
Yukuro <tupledev9@gmail.com>
2019-10-30 11:26:54 +0900

    [Add] get sensor

--- Avionics/src/main.cpp
+++ Avionics/src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <I2Cdev.h>
+#include <MPU6050.h>
+
+MPU6050 acsensor;
 
 void setup() {
-  // put your setup code here, to run once:
+    Wire.begin();
+    Serial.begin(9600);
+
+    acsensor.initialize();
+    pinMode(13, OUTPUT);
+    Serial.println("AC Initialize done!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    int16_t accelx = getAccelerationX(), accely = getAccelerationY(), accelz = getAccelerationZ();
+    double accelres = sqrt(accelx*accelx + accely*accely + accelz*accelz);
+    Serial.print(" ACX:"); Serial.print(accelx);
+    Serial.print(" ACY:"); Serial.print(accely);
+    Serial.print(" ACZ:"); Serial.print(accelz);
+    Serial.print(" RES:"); Serial.println(accelres);
 }
