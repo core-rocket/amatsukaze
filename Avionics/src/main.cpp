65178795b4defeca45fda012d6387851fe2bffb9
Yukuro <tupledev9@gmail.com>
2019-11-03 11:35:33 +0900

    [Add] get from BME280

--- Avionics/src/main.cpp
+++ Avionics/src/main.cpp
@@ -2,10 +2,32 @@
 #include <Wire.h>
 #include "SparkFunBME280.h"
 
+BME280 altsensor;
+
 void setup() {
-  // put your setup code here, to run once:
+  Wire.begin();
+  pinMode(13,OUTPUT);
+  Serial.begin(9600);
+
+  if(altsensor.beginI2C() == false){
+    Serial.println("[ERROR]BME280 init failed...");
+    while(1);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(13,HIGH);
+
+  float pressure, humidity, altitude;
+  pressure = altsensor.readFloatPressure();
+  humidity = altsensor.readFloatHumidity();
+  altitude = altsensor.readFloatAltitudeMeters();
+  Serial.print(" P:"); Serial.print(pressure,3);
+  Serial.print(" H:"); Serial.print(humidity,3);
+  Serial.print(" A:"); Serial.print(altitude,3);
+  Serial.print("\n");
+
+  delay(100);
+  digitalWrite(13,LOW);
+  delay(500);
 }
