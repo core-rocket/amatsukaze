aaf1bb9704969e19c1fc343c8de485debf59b704
Yukuro <tupledev9@gmail.com>
2019-11-07 01:24:41 +0900

    [Add] launch_by_accel

--- Avionics/src/main.cpp
+++ Avionics/src/main.cpp
@@ -4,7 +4,6 @@
 #include <MPU6050.h>
 #include "SparkFunBME280.h"
 #include "SparkFunBME280.h"
-MPU6050 acsensor;
 
 enum class Mode{
     standby,
@@ -15,7 +14,10 @@ enum class Mode{
 
 namespace global{
     Mode mode;
+    MPU6050 acsensor;
 }
+
+bool launch_by_accel();
 
 void setup() {
     Wire.begin();
@@ -23,14 +25,14 @@ void setup() {
 
     global::mode = Mode::standby;
 
-    acsensor.initialize();
+    global::acsensor.initialize();
     pinMode(13, OUTPUT);
     Serial.println("AC Initialize done!");
 }
 
 void loop() {
-    int16_t accelx = acsensor.getAccelerationX(), accely = acsensor.getAccelerationY(), accelz = acsensor.getAccelerationZ();
-    double accelres = sqrt(accelx*accelx + accely*accely + accelz*accelz);
+    //int16_t accelx = acsensor.getAccelerationX(), accely = acsensor.getAccelerationY(), accelz = acsensor.getAccelerationZ();
+    //double accelres = sqrt(accelx*accelx + accely*accely + accelz*accelz);
 
     switch (global::mode)
     {
@@ -38,6 +40,7 @@ void loop() {
             break;
 
         case Mode::flight:
+            if(launch_by_accel()) Serial.println("LaunchByACC_[SUCCESS]");
             break;
 
         case Mode::rise:
@@ -49,6 +52,34 @@ void loop() {
         default:
             break;
     }
+}
+
+bool launch_by_accel(){
+    int limit_counter = 0;
+    int average_rate = 5;
+    double average_buff[5] = {0.0};
+    double average;
+    for(int limit_index = 0; limit_index < 10; limit_index++){
+        for(int i = 0; i < average_rate - 1; i++){
+            average_buff[i] = average_buff[i+1]; //左にずらす
+        }
+        int16_t accelx = global::acsensor.getAccelerationX(), accely = global::acsensor.getAccelerationY(), accelz = global::acsensor.getAccelerationZ();
+        double accelres = sqrt(accelx*accelx + accely*accely + accelz*accelz);
+        average_buff[average_rate - 1] = accelres; //バッファの右端だけ新しい値を入れる
+
+        for(int i = 0; i < average_rate; i++){
+            double sum = average_buff[i];
+            average = sum / 5.0;
+        }
+
+        if(average >  30000){ //TODO: 本当に3G?
+            limit_counter++;
+        }else{
+            limit_counter = 0;
+        }
+    }
+    if(limit_counter >= 10) return true;
+    return false;
 }
 
 
