6fdd1c0709b535aeafdc025a88da8655a18d342d
Yukuro <tupledev9@gmail.com>
2019-11-07 23:43:15 +0900

    [Add] first template

--- Avionics/src/main.cpp
+++ Avionics/src/main.cpp
@@ -1,11 +1,42 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include "SparkFunBME280.h"
+
+enum class Mode{
+    standby,
+    flight,
+    rise,
+    parachute,
+};
+
+namespace global{
+    Mode mode;
+}
+
+/* プロトタイプ宣言書く場所 */
 
 void setup() {
-  // put your setup code here, to run once:
+    Wire.begin();
+    Serial.begin(9600);
+
+    global::mode = Mode::standby;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    switch (global::mode)
+    {
+        case Mode::standby:
+            break;
+
+        case Mode::flight:
+            break;
+
+        case Mode::rise:
+            break;
+
+        case Mode::parachute:
+            break;
+        
+        default:
+            break;
+    }
 }
