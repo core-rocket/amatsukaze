1c4cae6b66d9a99ed4a249a05347bcc8b4ffd543
Yukuro <tupledev9@gmail.com>
2019-11-08 00:17:49 +0900

    [Add] get value from GNSS

--- Avionics/src/main.cpp
+++ Avionics/src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
 
 enum class Mode{
     standby,
@@ -10,6 +12,13 @@ enum class Mode{
 
 namespace global{
     Mode mode;
+    static const int RXPin = 3, TXPin = 2;
+    static const uint32_t GPSBaud = 4800;
+    TinyGPSPlus gps;
+    SoftwareSerial ss(RXPin, TXPin);
+
+    double latitude = 0.0, longitude = 0.0;
+    uint8_t ephemeris_hour = 0, ephemeris_minute = 0, ephemeris_second = 0;
 }
 
 /* プロトタイプ宣言書く場所 */
@@ -17,11 +26,29 @@ namespace global{
 void setup() {
     Wire.begin();
     Serial.begin(9600);
+    global::ss.begin(global::GPSBaud);
 
     global::mode = Mode::standby;
 }
 
 void loop() {
+    if(global::gps.location.isValid()){
+        global::latitude = global::gps.location.lat();
+        global::longitude = global::gps.location.lng();
+    }
+    if(global::gps.time.isValid()){
+        global::ephemeris_hour = global::gps.time.hour();
+        global::ephemeris_minute = global::gps.time.minute();
+        global::ephemeris_second = global::gps.time.second();
+    }
+
+    Serial.print(global::latitude); Serial.print("/"); Serial.print(global::longitude);
+    Serial.print("\n");
+    Serial.print(global::ephemeris_hour); Serial.print(":");
+    Serial.print(global::ephemeris_minute); Serial.print(":");
+    Serial.print(global::ephemeris_second); 
+    Serial.print("\n");
+
     switch (global::mode)
     {
         case Mode::standby:
