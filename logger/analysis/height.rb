5ad755d4943f40eb787ac86da94ba11b60f7298c
sksat <sksat@sksat.net>
2019-12-04 15:32:11 +0900

    [add] calc height from log data script

---
+++ logger/analysis/height.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby
+
+require 'csv'
+
+csv = CSV.read(ARGV[0], headers: true)
+
+P0          = 1024.19   # 海面気圧
+HEIGHT_BASE = 425.2     # 射点高度
+
+max_height = 0.0
+top_time = 0.0
+
+csv.each do |data|
+  id = data["id"].to_i
+  time = data["time"].to_f
+  temp = data["temperature"].to_f
+  press= data["pressure"].to_f
+
+  height = (((P0/press)**(1/1.257) - 1) * (temp + 273.15)) / 0.0065
+  height = height - HEIGHT_BASE
+
+  if height == Float::INFINITY
+    next
+  end
+  if max_height < height
+    max_height = height
+    top_time = time
+  end
+
+  puts "#{time} #{temp} #{press} #{height}"
+end
+
+STDERR.puts "max height: #{max_height} (#{top_time} sec)"
